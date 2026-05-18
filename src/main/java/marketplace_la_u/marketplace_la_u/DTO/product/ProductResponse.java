aed58b0add44e3784ea6a61a63d37e536944d349
Mateo Canales <mateo.canales24@itca.edu.sv>
2026-05-17 22:43:43 -0600

    Corrección con traida de name de category del ProductResponse

--- src/main/java/marketplace_la_u/marketplace_la_u/DTO/product/ProductResponse.java
+++ src/main/java/marketplace_la_u/marketplace_la_u/DTO/product/ProductResponse.java
@@ -10,8 +10,11 @@ public class ProductResponse {
     private String description;
     private Double price;
     private Integer stock;
+    private Long categoryId;      // Agregado para el frontend
     private String categoryName;
     private String imgUrl;
+    private Long userId;          // Agregado para el frontend
+    private String userName;      // Agregado para mostrar el vendedor
 
     public ProductResponse(Product product) {
         this.id = product.getId();
@@ -19,7 +22,12 @@ public class ProductResponse {
         this.description = product.getDescription();
         this.price = product.getPrice().doubleValue();
         this.stock = product.getStock();
+        this.categoryId = product.getCategory().getId();
         this.categoryName = product.getCategory().getName();
         this.imgUrl = product.getImg_url();
+
+        // Extraemos los datos del usuario que creó el producto
+        this.userId = product.getUser().getId();
+        this.userName = product.getUser().getName();
     }
 }
